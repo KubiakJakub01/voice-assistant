f363f53fbc255d8e9131552f6d7d5394cf3aa3a5
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-05-25 21:31:53 +0200

    Move config/settings.py -> settings.py

--- src/config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Config package for the Gastronomy Voice Assistant.
-Contains restaurant data and configuration.
-"""
-
-from .settings import AGENT_INSTRUCTIONS, AGENT_NAME, DATA_FILE_PATH
-
-__all__ = ['AGENT_INSTRUCTIONS', 'AGENT_NAME', 'DATA_FILE_PATH']
