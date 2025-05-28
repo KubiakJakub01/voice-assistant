b4a71f6f8b6285e137ad380db86994dfb5721428
KubiakJakub01 <kubiak.jakub01@gmail.com>
2025-05-28 20:31:43 +0200

    Remove core package

--- src/core/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Core package for the Gastronomy Voice Assistant.
-Contains the voice pipeline and other core functionality.
-"""
-
-from .text_agent import TextAgent
-from .voice_agent import VoiceAgent
-
-__all__ = ['VoiceAgent', 'TextAgent']
